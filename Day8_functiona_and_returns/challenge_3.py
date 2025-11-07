81c59724ccccff9483743688fedffcc5d2a434bc
collansang <collankimtaisang@gmail.com>
2025-11-07 17:27:30 +0300

    functions and return

---
+++ Day8_functiona_and_returns/challenge_3.py
@@ -0,0 +1,11 @@
+# Create a function average_temp(temps) that:
+
+ # Accepts a list of temperatures (e.g. [36.5, 37.8, 38.0, 36.9])
+ # Calculates the average temperature
+ # Returns the result rounded to one decimal place
+
+def avg_temp(*temps):
+    average = sum(temps) / len(temps)
+    return round(average, 1)
+
+print(avg_temp(46, 46, 36.7, 34))
