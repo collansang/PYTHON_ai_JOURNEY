ad14b521f2004fc479a11ed00b5ddedfa6127f60
collansang <collankimtaisang@gmail.com>
2025-11-13 07:37:46 +0300

    csv files

---
+++ Day13_CSV_files/practice.py
@@ -0,0 +1,52 @@
+# #Exercise 1 — Read All Rows Correctly
+import csv
+# with open("patients.csv" , "r") as file:
+#     reader = csv.reader(file)
+#     for row in reader:
+#         print(row)
+
+
+#Exercise 2 — Use DictReader
+
+# with open("patients.csv", "r") as file:
+#     d_reader = csv.DictReader(file)
+#     for row in d_reader:
+#         print(row)
+
+
+#Exercise 4 — Write to a New CSV
+
+def check_temp(temp):
+    if temp >37.5:
+        return "fever"
+    elif temp <= 36:
+        return "low temperature"
+    else:
+        return "normal temperature"
+
+
+with open("patients.csv", "r") as file:
+    reader = csv.DictReader(file)
+
+    
+    names = ["name", "temperature", "condition"]
+    with open("summary.csv", "w", newline="") as out_file:
+       
+        writer = csv.DictWriter(out_file, fieldnames = names)
+        writer.writeheader()
+        
+        for row in reader:
+            name = row["name"]
+            temperature = float(row["temperature"])
+            row["condition"] = check_temp(temperature)#added a new row   
+            
+            filtered_row = {key:row[key] for key in writer.fieldnames}
+            writer.writerow(filtered_row)
+            
+            # writer.writerow({
+            #     "name" : row["name"],
+            #     "temperature" : row["temperature"],
+            #     "condition" : row["condition"]
+            # }) 
+
+
