9e8195c3657b6e863abc3eacae44fa7b1971dd2a
collansang <collankimtaisang@gmail.com>
2025-11-15 08:58:48 +0300

    csv files and a mini project

--- Day14_JSOn/week2_projectt/patient_mngnt_systm.py
+++ Day14_JSOn/week2_projectt/patient_mngnt_systm.py
@@ -24,7 +24,7 @@ def check_temp(temp):
 
 def load_data():
     if not os.path.exists(DATA_FILE):
-        return None
+        return []
     try:
         with open(DATA_FILE, "r") as file:
             return json.load(file)
@@ -39,34 +39,38 @@ def save_data(data):
         json.dump(data, file, indent=2)
  
 
-def add_patient(data):    
-            try:
-                name = input("enter the name: ").strip()
-                temp = float(input("enter temperature: "))
-                age = int(input("enter the age: "))
-                symptoms = input("Enter symptoms (comma separated): ")
-                symptoms_list = [s.strip() for s in symptoms.split(",") if s.strip()]
-            except ValueError as e:
-                print("Error", e) 
-                 
-            patient = {
-                "name" : name,
-                "age" : age,
-                "temperature" : temp,
-                "status" : check_temp(temp),
-                "symptoms" : symptoms_list        
-                }
+def add_patient(data):
+                try:
+                                      
+                    name = input("enter the name: ").strip()
+                    temp = float(input("enter temperature: "))
+                    age = int(input("enter the age: "))
+                    symptoms = input("Enter symptoms (comma separated): ")
+                    symptoms_list = [s.strip() for s in symptoms.split(",") if s.strip()]                  
+                
+                except ValueError:
+                     print("Invalid input. Patient not recorded")
                     
-            data.append(patient)
-            save_data(data)
-            print("patient successfully recorded")
+                patient = {
+                        "name" : name,
+                        "age" : age,
+                        "temperature" : temp,
+                        "status" : check_temp(temp),
+                        "symptoms" : symptoms_list        
+                        }    
+                        
+                data.append(patient)
+                save_data(data)
+                print("patient successfully recorded")
 
 
 def view_patient(data):
     if not data:
-          print("No patients found")
+          print("No patients found\n")
+          return
+    
     for p in data:
-         print(f"{p["name"]} - {p["age"]} - {p["temperature"]}℃ {p["status"]}")
+         print(f"{p['name']} - {p['age']} - {p['temperature']}℃ {p['status']}")
     print()
 
 
@@ -78,15 +82,61 @@ def search_patient(data):
           print("No patient found\n")
           return 
      p = results[0]
-     print(f"\nName:  {p["name"]}")
-     print(f"\nage:  {p["age"]}")
-     print(f"\ntemperature:  {p["temperature"]}")
-     print(f"\nstatus:  {p["status"]}")
-     print(f"\nsymptoms:  {', '.join(p["symptoms"])}")
+     print(f"\nName:  {p['name']}")
+     print(f"\nage:  {p['age']}")
+     print(f"\ntemperature:  {p['temperature']}")
+     print(f"\nstatus:  {p['status']}")
+     print(f"\nsymptoms:  {', '.join(p['symptoms'])}")
 
-def update_temp(data):
+def update_temperature(data):
      name = input("enter the patient name to update: ").strip().lower()
 
-     
-     
+     for p in data:
+          if p["name"].lower() == name:
+               try:
+                    
+                new_temp = float(input("enter the the new temperature: "))
+               except ValueError:
+                    print("invalid temperature")
+                    return
+               
+               p["temperature"] = new_temp
+               p["status"] = check_temp(new_temp)
+               save_data(data)
+               print("Temperature updated successfully.\n")
+               return
+          
+     print("Patient not found")
+
+def menu():
+     data = load_data()
+     while True:
+          print("===========================PATIENT MANAGER======================")
+          print("1. Add patient")
+          print("2. View all patients")
+          print("3.Search a patient")
+          print("4. Update a patient")
+          print("5. Exit/Quit")
+          print("===========================PATIENT MANAGER======================")
+          
+
+          choice = input("Choose option:").strip()
+
+          if choice == "1":
+               add_patient(data)
+          elif choice == "2":
+              view_patient(data)
+          elif choice == "3":
+              search_patient(data)
+          elif choice == "4":
+              update_temperature(data)
+          elif choice == "5":
+               break
+          else:
+               print("Invalid choice")
+               
+
+menu()
+
+
           
