d6a04ceff1e3525a9d6ef9405ecc00150df0c0f0
collansang <collankimtaisang@gmail.com>
2025-11-24 12:32:37 +0300

    repr

--- Day18_dunder_methods/sample.py
+++ Day18_dunder_methods/sample.py
@@ -10,13 +10,13 @@ class Patient:
                 f"weight={self.weight!r})")
 
 
-# ---- Broken dataset on purpose ----
+#Broken dataset on purpose
 patients = [
     Patient("Alex", 30, 70),
-    Patient("Mary", None, 55),        # BUG: age is None
-    Patient("John", "forty", 90),     # BUG: age is a string
-    Patient("", 22, 55),              # BUG: empty name
-    Patient("Sam", 25, -10),          # BUG: invalid negative weight
+    Patient("Mary", None, 55),        #age is None
+    Patient("John", "forty", 90),     #age is a string
+    Patient("", 22, 55),              #empty name
+    Patient("Sam", 25, -10),          #invalid negative weight
 ]
 
 
