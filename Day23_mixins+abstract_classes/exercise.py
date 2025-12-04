3bc1c3ebd6ba2b235ade871f6b44d0bf2cac2deb
collansang <collankimtaisang@gmail.com>
2025-12-04 18:28:24 +0300

    mini project,

--- Day23_mixins+abstract_classes/exercise.py
+++ Day23_mixins+abstract_classes/exercise.py
@@ -16,27 +16,26 @@ class LoggingMixin():
         print(f"LOG: {message}")
         
 class AgeValidator(BaseValidator, LoggingMixin):
-    
+    @property
     def name(self):
-        return "age validator"
+        return "AgeValidator"
     
-    def validate(self):
-        return "age valid"
-    pass
-
+    def validate(self, patient):
+        if patient.age  < 0 or patient.age >120:
+            raise ValueError("age out of range. Should be btw 0 and 120")
+        return True    
 
 class TemperatureValidator(BaseValidator):
+    @property
     def name(self):
-        return "temp validation"
+        return "TemperatureValidator"
     
-    def validate(self, Patient):
-        if Patient.temperature > 37.5:
-            return "fever"
-        elif Patient.temperature < 35:
-            return "hypothermia"
-        else:
-            return "normal"
-   
+    def validate(self, patient):
+        if not (34 <= patient.temperature <= 42):
+            raise ValueError("temperature out of range. Should be btw 34 and 42")
+        return True
+           
+           
 
 class Patient:
     def __init__(self, name, age, temperature):
@@ -44,14 +43,27 @@ class Patient:
         self.age = age
         self.temperature = temperature
         
-        def __repr__(self):
-            return f"Patient (name = {self.name},age = {self.age}), temperature = {self.temperature}"
-      
-      
-def run_validation(patient, validators: list):
+    def __repr__(self):
+        return f"Patient (name = {self.name},age = {self.age}), temperature = {self.temperature}"
+def run_validations(patient, validators: list):
     for validator in validators:
-        validate = validator.validate(patient)    
-        if validate is True:
-                print(f"{validator.name} OK")
-        else:
-            print(f"{validator.name} "+ validate)   
+      
+            if isinstance(validator, LoggingMixin):
+                validator.log(f"validating with {validator.name} ")
+            try:    
+                  validator.validate(patient)
+                  print(f"{validator.name}  ok") 
+            except ValueError as e:
+                print(f"{validator.name} FAILED: {e}")
+                
+        
+           
+
+
+patient1 = Patient("Collan", 21, 37)
+patient2 = Patient("John", 150, 39)
+patient3 = Patient("Doe", 25, 50)
+validators = [AgeValidator(), TemperatureValidator()]
+run_validations(patient1, validators)
+run_validations(patient2, validators)
+run_validations(patient3, validators)
